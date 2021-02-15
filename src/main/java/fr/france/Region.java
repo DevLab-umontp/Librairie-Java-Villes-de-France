2479cc03e30557a19f2c982bf9648432a4869cc0
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-02-16 00:35:20 +0100

    🔨 Ajout Region

---
+++ src/main/java/fr/france/Region.java
@@ -0,0 +1,21 @@
+package fr.france;
+
+public enum Region {
+    AUVERGNE_RHONE_ALPES, //
+    BOURGOGNE_FRANCHE_COMTE, //
+    BRETAGNE, //
+    CENTRE_VAL_DE_LOIRE, //
+    CORSE, //
+    GRAND_EST, GUADELOUPE, //
+    GUYANE, //
+    HAUTS_DE_FRANCE, //
+    ILE_DE_FRANCE, //
+    LA_REUNION, //
+    MARTINIQUE, //
+    MAYOTTE, //
+    NORMANDIE, //
+    NOUVELLE_AQUITAINE, //
+    OCCITANIE, //
+    PAYS_DE_LA_LOIRE, //
+    PROVENCE_ALPES_COTE_D_AZUR
+}
