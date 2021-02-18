9065d450c74a923ede608190b2970c76d62bdbd7
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-02-18 17:31:24 +0100

    🔨 Ajout des noms : Region.java

--- src/main/java/fr/france/Region.java
+++ src/main/java/fr/france/Region.java
@@ -3,25 +3,27 @@ package fr.france;
 import fr.france.outils.OutilsDepartement;
 
 public enum Region {
-    AUVERGNE_RHONE_ALPES, //
-    BOURGOGNE_FRANCHE_COMTE, //
-    BRETAGNE, //
-    CENTRE_VAL_DE_LOIRE, //
-    CORSE, //
-    GRAND_EST, GUADELOUPE, //
-    GUYANE, //
-    HAUTS_DE_FRANCE, //
-    ILE_DE_FRANCE, //
-    LA_REUNION, //
-    MARTINIQUE, //
-    MAYOTTE, //
-    NORMANDIE, //
-    NOUVELLE_AQUITAINE, //
-    OCCITANIE, //
-    PAYS_DE_LA_LOIRE, //
-    PROVENCE_ALPES_COTE_D_AZUR;
+    AUVERGNE_RHONE_ALPES("Auvergne-Rhône-Alpes"), //
+    BOURGOGNE_FRANCHE_COMTE("Bourgogne-Franche-Comté"), //
+    BRETAGNE("Bretagne"), //
+    CENTRE_VAL_DE_LOIRE("Centre-Val de Loire"), //
+    CORSE("Corse"), //
+    GRAND_EST("Grand Est"), //
+    GUADELOUPE("Guadeloupe"), //
+    GUYANE("Guyane"), //
+    HAUTS_DE_FRANCE("Hauts-de-France"), //
+    ILE_DE_FRANCE("Île-de-France"), //
+    LA_REUNION("La Réunion"), //
+    MARTINIQUE("Martinique"), //
+    MAYOTTE("Mayotte"), //
+    NORMANDIE("Normandie"), //
+    NOUVELLE_AQUITAINE("Nouvelle-Aquitaine"), //
+    OCCITANIE("Occitanie"), //
+    PAYS_DE_LA_LOIRE("Pays de la Loire"), //
+    PROVENCE_ALPES_COTE_D_AZUR("Provence-Alpes-Côte d'Azur");
 
     private Departement[] departements;
+    private String nom;
 
     static {
         for (Region region : Region.values()) {
@@ -30,9 +32,23 @@ public enum Region {
     }
 
     /**
+     * @param nom
+     */
+    private Region(String nom) {
+        this.nom = nom;
+    }
+
+    /**
      * @return the departements
      */
     public Departement[] getDepartements() {
         return departements;
+    }
+
+    /**
+     * @return the nom
+     */
+    public String getNom() {
+        return nom;
     }
 }
